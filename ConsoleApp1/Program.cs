f980d189955ea4b0ed6e26b70cbb4cf5e59bdc57
TheBlueSoul5188 <hibba04@hotmail.se>
2022-09-23 10:56:06 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            Console.Write("Svara här: "); int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder <= 19 && ålder >= 16)
+            {
+                Console.WriteLine("Du får delta i tävlingen.");
+            }
+
+            else
+            {
+                Console.WriteLine("Du får inte delta i tävlingen.");
+            }
+        }
+    }
+}
